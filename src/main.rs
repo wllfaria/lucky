0eaaa38b4a66349c4a579cab8982d76ede9dbfb7
Willians Faria <dev.willians.faria@gmail.com>
2024-04-06 18:25:56 -0300

    feat: connecting to X server with a cursor

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod lucky;
+
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("failed to set global subscriber");
+
+    lucky::Lucky::new().start();
+}
