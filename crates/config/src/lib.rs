fff80c680510cb592b312dabd987c4a5bdc255ef
Willians Faria <dev.willians.faria@gmail.com>
2024-05-17 18:38:20 -0300

    feat: adding logging to file

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -11,20 +11,35 @@ static APP_NAME: &str = "lucky";
 static CONFIG_FILE: &str = "config.toml";
 static XDG_HOME: &str = "HOME";
 static XDG_CONFIG_HOME: &str = "XDG_CONFIG_HOME";
+static XDG_DATA_DIR: &str = "XDG_DATA_HOME";
 static LUCKY_CONF_ENV_VAR: &str = "LUCKY_CONFIG";
 
 /// Verify if `$HOME`/.config/lucky/config.toml exists
 fn get_config_dir_path() -> Option<PathBuf> {
     let var = match std::env::var(XDG_CONFIG_HOME) {
-        Ok(config_path) => Some(Path::new(&config_path).join(APP_NAME).join(CONFIG_FILE)),
+        Ok(config_path) => {
+            tracing::debug!(
+                "loading config file from $XDG_CONFIG_HOME: {config_path}/lucky/config.toml"
+            );
+            Some(Path::new(&config_path).join(APP_NAME).join(CONFIG_FILE))
+        }
         Err(_) => match std::env::var(XDG_HOME) {
-            Ok(home_path) => Some(
-                Path::new(&home_path)
-                    .join(".config")
-                    .join(APP_NAME)
-                    .join(CONFIG_FILE),
-            ),
-            Err(_) => None,
+            Ok(home_path) => {
+                tracing::debug!(
+                    "loading config file from $HOME: {home_path}/.config/lucky/config.toml"
+                );
+                Some(
+                    Path::new(&home_path)
+                        .join(".config")
+                        .join(APP_NAME)
+                        .join(CONFIG_FILE),
+                )
+            }
+
+            Err(_) => {
+                tracing::debug!("no config file found, loading default");
+                None
+            }
         },
     };
     var
@@ -56,10 +71,13 @@ where
 /// * If not present on any of the directories above, will load the default configuration;
 pub fn load_config() -> Config {
     let config_path = match std::env::var(LUCKY_CONF_ENV_VAR) {
-        Ok(var) => Some(PathBuf::from(&var).join(CONFIG_FILE)),
+        Ok(var) => {
+            tracing::debug!("loading config file from $LUCKY_CONFG: {var:?}");
+            Some(PathBuf::from(&var).join(CONFIG_FILE))
+        }
         Err(_) => get_config_dir_path(),
     };
-    match config_path
+    let config = match config_path
         .map(load_config_from_file)
         .unwrap_or(Ok(Config::default()))
     {
@@ -68,5 +86,35 @@ pub fn load_config() -> Config {
             tracing::error!("{e:?}");
             Config::default()
         }
-    }
+    };
+
+    tracing::debug!("loaded config with {} actions", config.actions.len());
+    tracing::debug!("loaded config with {} commands", config.commands.len());
+
+    config
+}
+
+fn data_dir() -> anyhow::Result<PathBuf> {
+    let data_path = match std::env::var(XDG_DATA_DIR) {
+        Ok(data_path) => PathBuf::from(data_path).join(APP_NAME),
+        Err(_) => match std::env::var(XDG_HOME) {
+            Ok(home_path) => {
+                let data_path = PathBuf::from(home_path)
+                    .join(".local")
+                    .join("share")
+                    .join(APP_NAME);
+                if !data_path.is_dir() {
+                    std::fs::create_dir(&data_path)?;
+                }
+                data_path
+            }
+            Err(_) => anyhow::bail!("failed to get $HOME environment variable"),
+        },
+    };
+
+    Ok(data_path)
+}
+
+pub fn log_file() -> anyhow::Result<(PathBuf, String)> {
+    Ok((data_dir()?, format!("{}.log", APP_NAME)))
 }
--- crates/lucky/src/main.rs
+++ crates/lucky/src/main.rs
@@ -9,14 +9,18 @@ mod screen;
 mod screen_manager;
 
 use lucky::Lucky;
-use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
 fn main() -> anyhow::Result<()> {
+    let (data_dir, log_file) = config::log_file()?;
+    let log_writer = tracing_appender::rolling::daily(data_dir, log_file);
+    let (non_blocking, _guard) = tracing_appender::non_blocking(log_writer);
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
+        .with_max_level(tracing::Level::TRACE)
+        .with_ansi(false)
+        .with_writer(non_blocking)
         .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("failed to set global subscriber");
+    tracing::subscriber::set_global_default(subscriber).expect("Setting default subscriber failed");
 
     Lucky::new().run();
 
